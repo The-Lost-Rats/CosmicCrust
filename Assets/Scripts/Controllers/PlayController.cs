b0fcdc635fb63a0d8727e904adf84bf0c6944bb9
Justin Roderman <jcroderman@gmail.com>
2022-10-02 05:48:33 -0700

    Adds pizza topping images and enums and creates play controller

---
+++ Assets/Scripts/Controllers/PlayController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayController : MonoBehaviour
+{
+    public static PlayController instance = null;
+
+    public Pizza pizzaPrefab;
+
+    [SerializeField] private List<PizzaOrder> pizzaOrders = null;
+
+    private int numOrdersCompleted;
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+        InitalizeGame();
+    }
+
+    private List<int> ValidatePizzaOrders()
+    {
+        List<int> invalidLevels = new List<int>();
+        for (int i = 0; i < pizzaOrders.Count; i++)
+        {
+            PizzaOrder order = pizzaOrders[i];
+            if (order != null)
+            {
+                int numIngredients = order.meats.Count + order.peppers.Count +
+                    order.vegetables.Count + order.genericToppings.Count + (order.hasPineapple ? 1 : 0);
+                if (numIngredients > Constants.maxIngredients)
+                {
+                    invalidLevels.Add(i);
+                }
+            }
+            else
+            {
+                invalidLevels.Add(i);
+            }
+        }
+        return invalidLevels;
+    }
+
+    private void InitalizeGame()
+    {
+        List<int> invalidLevels = ValidatePizzaOrders();
+        if (invalidLevels.Count > 0)
+        {
+            string invalidLevelsStr = "";
+            foreach (int level in invalidLevels)
+            {
+                invalidLevelsStr += (invalidLevelsStr != "" ? ", " : "") + level;
+            }
+            Debug.LogError("Invalid levels! " + invalidLevelsStr);
+            return;
+        }
+
+        // Initialize the game
+        numOrdersCompleted = 0;
+    }
+}
--- Assets/Scripts/GameObjects/ConveyorBelt/Pizza.cs
+++ Assets/Scripts/GameObjects/ConveyorBelt/Pizza.cs
@@ -7,6 +7,56 @@ public class Pizza : InteractableObject
     public override bool isInteractable { get { return false; }}
     public override List<string> interactableObjects { get { return new List<string>{}; }}
 
+    [System.Serializable]
+    public struct SauceImage
+    {
+        public Constants.Sauces sauce;
+        public Texture2D image;
+    }
+    [SerializeField] public List<SauceImage> sauceImages;
+
+    [System.Serializable]
+    public struct CheeseImage
+    {
+        public Constants.CheeseTypes cheese;
+        public Texture2D image;
+    }
+    [SerializeField] public List<CheeseImage> cheeseImages;
+
+    [System.Serializable]
+    public struct MeatImage
+    {
+        public Constants.Meats meat;
+        public Texture2D image;
+    }
+    [SerializeField] public List<MeatImage> meatImages;
+
+    [System.Serializable]
+    public struct PepperImage
+    {
+        public Constants.Peppers pepper;
+        public Texture2D image;
+    }
+    [SerializeField] public List<PepperImage> pepperImages;
+
+    [System.Serializable]
+    public struct VegetableImage
+    {
+        public Constants.Vegetables vegetable;
+        public Texture2D image;
+    }
+    [SerializeField] public List<VegetableImage> vegetableImages;
+
+    [System.Serializable]
+    public struct ToppingImage
+    {
+        public Constants.GenericToppings topping;
+        public Texture2D image;
+    }
+    [SerializeField] public List<ToppingImage> toppingImages;
+
+    [SerializeField] public Texture2D pineappleImage;
+    
     public void AddTopping(string topping)
     {
         Debug.Log("Adding topping " + topping);
