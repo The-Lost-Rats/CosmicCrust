d0165615c632dc602e9853cc6aca41be40364510
Justin Roderman <jcroderman@gmail.com>
2022-10-02 12:55:56 -0700

    Adding Cabinets! (#14)
    * Adds basic cabinets to the scene and allows you to open them
    
    * Fixes onMouseEnter 'bug' where cabinets were being confusingly interacted with
    
    * Adds stuff in the drawers

---
+++ Assets/Scripts/GameObjects/Cabinets/CabinetDrawer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CabinetDrawer : InteractableObject
+{
+    public override bool isInteractable { get { return true; }}
+    public override List<string> interactableObjects { get { return new List<string>{}; }}
+
+    private const float drawerHeight = .8f;
+
+    [SerializeField] private Constants.GenericToppings topping;
+
+    private SpriteRenderer sr;
+
+    private bool isMoving;
+    private float mouseOffset;
+    private float[] movementBounds;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        movementBounds = new float[]{transform.position.y - drawerHeight, transform.position.y};
+    }
+
+    public override void OnEnter()
+    {
+        sr.color = new Color(0.9f, 0.9f, 0.9f);
+    }
+
+    public override void OnExit()
+    {
+        sr.color = Color.white;
+    }
+
+    public override InputController.InputState OnClick()
+    {
+        isMoving = true;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseOffset = transform.position.y - mousePosition.y;
+        return InputController.InputState.Grabbing;
+    }
+ 
+    public override InputController.InputState OnRelease(List<InteractableObject> interactedObjects)
+    {
+        isMoving = false;
+        return InputController.InputState.Default;
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // Add an offset so the anchor point of the sprite can be ignored
+            float posY = Mathf.Clamp(mousePosition.y + mouseOffset, movementBounds[0], movementBounds[1]);
+            transform.position = new Vector2(transform.position.x, posY);
+        }
+    }
+}
---
+++ Assets/Scripts/GameObjects/Cabinets/DrawerTopping.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerTopping : InteractableObject
+{
+    public override bool isInteractable { get { return true; }}
+    public override List<string> interactableObjects { get { return new List<string>{ "Pizza" }; }}
+
+    public DroppableObject toppingPrefab;
+    private DroppableObject currInstance;
+
+    [SerializeField] private Constants.GenericToppings toppingType = Constants.GenericToppings.Anchovy;
+
+    private Vector3 originalScale = Vector3.zero;
+
+    public override void OnEnter()
+    {
+        // store old scale and mult by 1.1
+        originalScale = transform.localScale;
+        transform.localScale = originalScale * 1.1f;
+    }
+
+    public override void OnExit()
+    {
+        transform.localScale = originalScale;
+    }
+
+    public override InputController.InputState OnClick()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        currInstance = Instantiate(toppingPrefab, mousePosition, Quaternion.identity);
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        SpriteRenderer toppingRenderer = currInstance.GetComponent<SpriteRenderer>();
+        Texture2D currTexture = sr.sprite.texture;
+        toppingRenderer.sprite = Sprite.Create(currTexture, new Rect(0, 0, currTexture.width, currTexture.height), new Vector2(0.5f, 0.5f));
+        return InputController.InputState.Grabbing;
+    }
+
+    public override InputController.InputState OnRelease(List<InteractableObject> interactedObjects)
+    {
+        bool onPizza = false;
+        foreach (InteractableObject interactable in interactedObjects)
+        {
+            if (interactable.name == "Pizza")
+            {
+                PlayController.instance.AddGenericTopping(toppingType);
+                onPizza = true;
+            }
+        }
+        currInstance.Drop(onPizza);
+        currInstance = null;
+        return InputController.InputState.Default;
+    }
+}
