95c8009f6ec24c2a10e427461da5be2822ec7bbf
Justin Roderman <jcroderman@gmail.com>
2022-10-01 18:05:46 -0700

    Finalizes conveyor belt looping (#3)
    Co-authored-by: Varun Chadha <vichadha@protonmail.com>

--- Assets/Scripts/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class ConveyorBelt : MonoBehaviour
 {
+    public GameObject beltPrefab;
+
+    private const float beltTopBound = 1;
+    private const float beltBottomBound = -9;
+    private const float newBeltOffset = 12.15f;
+
     [Range(1, 5)]
     [SerializeField]
     private float speed = 1;
@@ -11,6 +17,7 @@ public class ConveyorBelt : MonoBehaviour
     void Update()
     {
         Move();
+        CheckBelt();
     }
 
     void Move()
@@ -20,8 +27,32 @@ public class ConveyorBelt : MonoBehaviour
             if (child.tag == "ConveyorBeltMovable")
             {
                 Vector3 pos = child.position;
-                pos.y -= 0.001f * speed;
+                pos.y -= 0.002f * speed;
                 child.position = pos;
+            }
+        }
+    }
+
+    void CheckBelt()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.tag == "ConveyorBeltMovable")
+            {
+                Belt beltData = child.GetComponent<Belt>();
+                if (beltData != null)
+                {
+                    if (!beltData.spawnedNextBelt && child.localPosition.y <= beltTopBound)
+                    {
+                        Instantiate(beltPrefab, child.position + new Vector3(0, newBeltOffset), Quaternion.identity, transform);
+                        beltData.spawnedNextBelt = true;
+                    }
+                }
+
+                if (child.localPosition.y <= beltBottomBound)
+                {
+                    GameObject.Destroy(child.gameObject);
+                }
             }
         }
     }
