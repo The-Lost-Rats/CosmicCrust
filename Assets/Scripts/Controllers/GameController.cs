ee1888a2f0451be19f38fbbea17c2a355a3c3757
Varun Chadha <vichadha@protonmail.com>
2022-10-01 18:02:15 -0700

    Implement basic pause menu (#2)
    * Add main menu scene and update build settings
    
    Added basic main menu scene. Also updated build settings to
    include both scenes.
    
    * Add pause menu to game scene
    
    Added pause menu (kinda janky right now) and updated build settings

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+    public static GameController instance = null;
+
+
+    public bool gameOver = false;
+    private bool isRunning;
+
+    private SceneController.Level currentLevel;
+
+    void Start() {
+        if (instance == null) {
+            instance = this;
+        }
+        else if (instance != this) {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+        // Start with main game level
+        SceneController.LoadLevel( SceneController.Level.MAIN_LEVEL );
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
+    }
+
+    void Update() {
+        switch( SceneController.GetCurrentLevel() ) {
+            case SceneController.Level.MAIN_LEVEL:
+                RunGame();
+                break;
+            case SceneController.Level.PAUSE_MENU:
+                RunPauseMenu();
+                break;
+        }
+    }
+
+    void RunPauseMenu() {
+        if ( Input.GetKeyDown( KeyCode.Escape ) ) {
+            ExitPauseMenu();
+        }
+    }
+
+    void RunGame() {
+        if ( Input.GetKeyDown( KeyCode.Escape ) ) {
+            SceneController.LoadLevel( SceneController.Level.PAUSE_MENU );
+            SceneManager.LoadScene("PauseMenuScene", LoadSceneMode.Additive);
+        }
+    }
+
+    private void OnSceneUnloaded( Scene unloadedScene ) {
+        if ( unloadedScene.name == "PauseMenuScene" ) {
+            ExitPauseMenu();
+        }
+    }
+
+    private void ExitPauseMenu() {
+        SceneController.LoadLevel( SceneController.Level.MAIN_LEVEL );
+        SceneManager.UnloadSceneAsync("PauseMenuScene");
+    }
+}
---
+++ Assets/Scripts/Controllers/SceneController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+    public static SceneController scInstance = null;
+
+    public enum Level {
+        MAIN_MENU,
+        MAIN_LEVEL,
+        PAUSE_MENU
+    };
+
+    private static Level currLevel;
+
+    public void Awake() {
+        if ( null == scInstance ) {
+            scInstance = this;
+            DontDestroyOnLoad( this.gameObject );
+        } else {
+            Destroy( this.gameObject );
+        }
+    }
+
+    public void Update() {}
+
+    public static Level GetCurrentLevel() {
+        return ( currLevel );
+    }
+
+    public static void LoadLevel( Level level ) {
+        currLevel = level;
+    }
+}
