e2c80062a140ec8da534bae8f986e46e8ac87e78
Justin Roderman <jcroderman@gmail.com>
2022-10-02 19:41:02 -0700

    Adds sauce machine! (#23)

---
+++ Assets/Scripts/GameObjects/SauceMachine/SauceHandle.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SauceHandle : InteractableObject
+{
+    public override bool isInteractable { get { return true; }}
+    public override List<string> interactableObjects { get { return new List<string>{}; }}
+
+    public Gradient marinaraGradient;
+    public Gradient alfredoGradient;
+    public Gradient bbqGradient;
+
+    public ParticleSystem sauceParticles;
+    public SauceWheel sauceWheel;
+
+    private float[] movementBounds = new float[]{ -2, 0.5f };
+    private float[] saucePositions;
+
+    private const int numSauces = 3;
+
+    private bool isMoving;
+    private float mouseOffset;
+
+    private Constants.Sauces currSauce = Constants.Sauces.Marinara;
+    private Constants.Sauces[] sauces = new Constants.Sauces[]{
+        Constants.Sauces.Marinara,
+        Constants.Sauces.Alfredo,
+        Constants.Sauces.BBQ
+    };
+
+    private void Start()
+    {
+        float step = (movementBounds[1] - movementBounds[0]) / numSauces;
+        saucePositions = new float[numSauces];
+        for (int i = 0; i < numSauces; i++)
+        {
+            saucePositions[i] = movementBounds[1] - ((step * i) + (step / 2));
+        }
+
+        SetSauce();
+    }
+
+    public override void OnEnter()
+    {
+        transform.localScale = new Vector3(1.1f, 1.1f);
+    }
+
+    public override void OnExit()
+    {
+        transform.localScale = Vector3.one;
+    }
+
+    public override InputController.InputState OnClick()
+    {
+        isMoving = true;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseOffset = transform.position.y - mousePosition.y;
+        return InputController.InputState.Grabbing;
+    }
+ 
+    public override InputController.InputState OnRelease(List<InteractableObject> interactedObjects)
+    {
+        isMoving = false;
+        SetSauce();
+        return InputController.InputState.Default;
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // Add an offset so the anchor point of the sprite can be ignored
+            float posY = Mathf.Clamp(mousePosition.y + mouseOffset, movementBounds[0], movementBounds[1]);
+            transform.position = new Vector2(transform.position.x, posY);
+        }
+    }
+
+    private void SetSauce()
+    {
+        int closestSauce = 0;
+        float minSauceDistance = Mathf.Infinity;
+        for (int i = 0; i < numSauces; i++)
+        {
+            float delta = Mathf.Abs(transform.position.y - saucePositions[i]);
+            if (delta < minSauceDistance)
+            {
+                closestSauce = i;
+                minSauceDistance = delta;
+            }
+        }
+        transform.position = new Vector2(transform.position.x, saucePositions[closestSauce]);
+        currSauce = sauces[closestSauce];
+        sauceWheel.currSauce = currSauce;
+
+        ParticleSystem.MainModule particleSystemMain = sauceParticles.main;
+        switch (currSauce)
+        {
+            case Constants.Sauces.Marinara:
+                particleSystemMain.startColor = marinaraGradient;
+                break;
+            case Constants.Sauces.Alfredo:
+                particleSystemMain.startColor = alfredoGradient;
+                break;
+            case Constants.Sauces.BBQ:
+                particleSystemMain.startColor = bbqGradient;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/GameObjects/SauceMachine/SauceWheel.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SauceWheel : InteractableObject
+{
+    public ParticleSystem sauceParticles;
+    public Bounds bounds;
+
+    public override bool isInteractable { get { return true; }}
+    public override List<string> interactableObjects { get { return new List<string>{}; }}
+
+    private bool isGrabbed;
+    private float angleOffset;
+
+    private float lastAngle;
+    private const float angleDiff = 5;
+
+    [SerializeField]
+    [Range(0.1f, 0.5f)]
+    private const float turnTimerStart = 0.2f;
+    private float turnTimer = 0;
+
+    [SerializeField]
+    [Range(1, 5)]
+    private float sauceAddedTime = 1;
+    private float sauceTimer = 0;
+    private bool sauceAdded = false;
+
+    public Constants.Sauces currSauce = Constants.Sauces.Marinara; // Set by SauceHandle
+
+    public override void OnEnter()
+    {
+        transform.localScale = new Vector3(1.1f, 1.1f);
+    }
+
+    public override void OnExit()
+    {
+        transform.localScale = Vector3.one;
+    }
+
+    public override InputController.InputState OnClick()
+    {
+        isGrabbed = true;
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition - transform.position;
+        angleOffset = Vector2.SignedAngle(Vector2.right, direction) - transform.eulerAngles.z;
+        lastAngle = angleOffset;
+        return InputController.InputState.Grabbing;
+    }
+ 
+    public override InputController.InputState OnRelease(List<InteractableObject> interactedObjects)
+    {
+        isGrabbed = false;
+        sauceParticles.Stop();
+        turnTimer = 0;
+        sauceTimer = 0;
+        sauceAdded = false;
+        return InputController.InputState.Default;
+    }
+
+    private void Update()
+    {
+        if (isGrabbed)
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = mousePosition - transform.position;
+            float angle = Vector2.SignedAngle(Vector2.right, direction) - angleOffset;
+            transform.eulerAngles = new Vector3 (0, 0, angle);
+
+            CheckForSpray();
+            if (turnTimer > 0)
+            {
+                turnTimer -= Time.deltaTime;
+                if (turnTimer <= 0)
+                {
+                    sauceParticles.Stop();
+                }
+                else if (bounds.pizzaInBounds)
+                {
+                    sauceTimer += Time.deltaTime;
+                    if (!sauceAdded && sauceTimer >= sauceAddedTime)
+                    {
+                        PlayController.instance.SetSauce(currSauce);
+                        sauceAdded = true;
+                    }
+                }
+            }
+
+            lastAngle = angle;
+        }
+    }
+
+    private void CheckForSpray()
+    {
+        if (Mathf.Abs(lastAngle - transform.eulerAngles.z) > angleDiff)
+        {
+            if (!sauceParticles.isPlaying)
+            {
+                sauceParticles.Play();
+            }
+            turnTimer = turnTimerStart;
+        }
+    }
+}
